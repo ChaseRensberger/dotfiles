d3d6553c929e447d25dbeddb437afd8ff79c58a0
ChaseRensberger <chasehrensberger@gmail.com>
2025-02-07 18:40:22 -0500

    minor cleaning

--- nvim/init.lua
+++ nvim/init.lua
@@ -137,7 +137,7 @@ cmp.setup({
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "tailwindcss", "ts_ls", "gopls", "html", "cssls", "htmx", "rust_analyzer" },
+	ensure_installed = { "lua_ls", "tailwindcss", "ts_ls", "gopls", "html", "cssls", "rust_analyzer" },
 	automatic_installation = true,
 })
 
@@ -158,33 +158,13 @@ require("lspconfig")["lua_ls"].setup({
 	},
 })
 
-require("lspconfig")["rust_analyzer"].setup({
-	capabilities = capabilities,
-})
+local servers_with_defaults = { "rust_analyzer", "ts_ls", "tailwindcss", "gopls", "html", "cssls" }
 
-require("lspconfig")["ts_ls"].setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig")["tailwindcss"].setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig")["gopls"].setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig")["html"].setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig")["cssls"].setup({
-	capabilities = capabilities,
-})
-
-require("lspconfig")["htmx"].setup({
-	capabilities = capabilities,
-})
+for _, server in ipairs(servers_with_defaults) do
+	require("lspconfig")[server].setup({
+		capabilities = capabilities,
+	})
+end
 
 require("conform").setup({
 	formatters_by_ft = {
